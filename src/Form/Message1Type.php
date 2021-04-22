c0d29cce6c46d075af6dfee9b5324df311fcd5b3
VicSpl <victoria.spieler@gmail.com>
2021-04-22 04:23:32 +0200

    - Création de ContactController - Ajout de la class bootstrap sur champs message - Ajout CollectionType pour rôle admin, génère champs auto pour tableau - affichage des coordonnées propriétaires des chats -  Ajout émeteur et destinataire dans l'espace message de l'admin - Enlever le active dans show - Création formulaire contacte - lien recieved et sent dans new message - Ajout de la messagerie et de contacte dans navbar

--- src/Form/Message1Type.php
+++ src/Form/Message1Type.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Message;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,8 +14,8 @@ class Message1Type extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('subject')
-            ->add('content', TextareaType::class)
+            ->add('subject', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('content', TextareaType::class, array('attr' => array('class' => 'form-control')))
             // ->add('sender')
             // ->add('recipient')
         ;
