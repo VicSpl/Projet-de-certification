d41a328d69e7efbcfe3c747a5625458b1c3f9858
VicSpl <victoria.spieler@gmail.com>
2021-05-09 21:32:49 +0200

    - Définition homepage - Ajout des cards de chat pour l'inspection - Ajout de la pop-up de refus (css/js)

---
+++ public/js/pop-up.js
@@ -0,0 +1,94 @@
+/* <div class="pop-up-shadow">
+    <div class="pop-up p-3">
+        <h4 class="pop-up-title">
+            Veuillez saisir le motif du refus :
+                </h4>
+        <textarea name="" id="" cols="65" rows="5" class="form-control" placeholder="Motif"></textarea>
+        <div class="btn-container mt-3">
+            <button class="btn btn-danger">
+                Refuser
+                    </button>
+            <button class="btn btn-secondary ml-3">
+                Annuler
+                    </button>
+        </div>
+    </div> 
+</div>*/
+
+/**
+ * buttonOk {text, fuct}
+ */
+
+/*
+Création de toutes les balises
+*/
+function popUp(title, buttonOk, buttonCancel, comment) {
+    let shadow = document.createElement("div");
+    let popUp = document.createElement("div");
+    let entitled = document.createElement("h4");
+    let textarea = document.createElement("textarea");
+    let buttonContainer = document.createElement("div");
+    let btnDanger = document.createElement("button");
+    let btnSecondary = document.createElement("button");
+
+
+    /*
+     Ajout de shadow dans le body.
+     */
+    shadow = document.body.appendChild(shadow);
+    /*
+    Ajout les class et text de ces balises. Ajouter les écouteurs d'évenement sur les click des boutons.
+    */
+
+    shadow.className = "pop-up-shadow";
+    popUp.className = "pop-up p-3";
+    entitled.className = "pop-up-title";
+    entitled.innerHTML = title;
+    textarea.className = "form-control";
+    buttonContainer.className = "btn-container mt-3";
+    btnDanger.className = "btn btn-danger";
+    btnDanger.innerHTML = buttonOk.text;
+    btnDanger.addEventListener("click", function () {
+        buttonOk.fuct();
+        // suppression de la pop-up
+        shadow.parentElement.removeChild(shadow);
+    })
+    btnSecondary.className = "btn btn-secondary ml-3"; btnSecondary.innerHTML = buttonCancel.text;
+
+    btnSecondary.addEventListener("click", function () {
+        buttonCancel.fuct();
+        // suppression de la pop-up
+        shadow.parentElement.removeChild(shadow);
+    })
+
+    /*
+    Construction de l'arborescence des balises. 
+    */
+
+    buttonContainer.appendChild(btnDanger);
+    buttonContainer.appendChild(btnSecondary);
+
+    popUp.appendChild(entitled);
+    popUp.appendChild(textarea);
+    popUp.appendChild(buttonContainer);
+    shadow.appendChild(popUp);
+
+
+}
+
+function refuseBtn(id) {
+
+    popUp("Veuillez saisir le motif du refus :", {
+        text: "Refuser",
+        fuct: function () {
+            console.log("le button refusé est cliqué");
+            // envoyer requête au serveur
+            
+        }
+    }, {
+        text: "Annuler",
+        fuct: function () {
+            console.log("le button annuler est cliqué")
+        }
+    })
+}
--- src/Controller/ReviewCatController.php
+++ src/Controller/ReviewCatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CatRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,12 @@ class ReviewCatController extends AbstractController
     /**
      * @Route("/review/cat", name="review_cat")
      */
-    public function index(): Response
+    public function index(CatRepository $catRepository): Response
     {
         return $this->render('review_cat/index.html.twig', [
-            'controller_name' => 'ReviewCatController',
+            'cats' => $catRepository->findBy([
+                'review' => null
+            ])
         ]);
     }
 }
