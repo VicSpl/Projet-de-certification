107837dd669d439d2dbd376c3786ba78eab51063
VicSpl <victoria.spieler@gmail.com>
2021-05-26 01:15:51 +0200

    Récupérer tout les chats :  - qui ont eu une validation, bonne ou mauvaise.  -  cacher la messagerie pour le représentant  -  Changement historique des inspections - ajout de la date dans les messages + gestion auto de la date - gestion du tableu de valeur dans les roles des utilisateurs - Changement du nom du bouton : -  de creation d'un nouveau message de save à envoyer - Refonte de la messagerie interne : - Passage en français - Ajout de nouveau champs ( relation ex : qui à envoyer etc. - Ajout des conditions pour gérer l'affichage de l'émetteur et récepteur sur une seule vue

--- src/Controller/ReviewHistoryController.php
+++ src/Controller/ReviewHistoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CatRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,12 @@ class ReviewHistoryController extends AbstractController
     /**
      * @Route("/review/history", name="review_history")
      */
-    public function index(): Response
+    public function index(CatRepository $catRepository): Response
     {
         return $this->render('review_history/index.html.twig', [
-            'controller_name' => 'ReviewHistoryController',
+            'cats' => $catRepository->findBy([
+                'review' => !null
+            ])
         ]);
     }
 }
